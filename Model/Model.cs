a8c3791ce10d2e1b26a54b292cbdb166651a5d09
CunXXX <ctc050966@gmail.com>
2025-04-26 18:13:45 +0800

    feat : 新增註冊頁面

---
+++ Model/Model.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model;
+
+public class RegisterInputModel
+{
+    [Required(ErrorMessage = "帳號為必填")] public string Account { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "密碼為必填")] public string Password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "請再次確認密碼")] public string ConfirmPassword { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email 為必填")][EmailAddress(ErrorMessage = "Email 格式不正確")] public string Email { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    [Required] public bool AgreePrivacy { get; set; }
+    [Required] public bool AgreeTerms { get; set; }
+}
