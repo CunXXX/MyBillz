0ddf0f08732fdd27d1b75eb5e10a0a7b395a088f
taicun <USER@DESKTOP-UCHUEK9>
2025-04-28 14:12:35 +0800

    feat : 新增功能介紹頁面

---
+++ FrondEnd/MyBillz/MyBillz/Pages/Features.razor.js
@@ -0,0 +1,15 @@
+﻿
+export function init() {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = 1;
+                entry.target.style.transform = 'translateY(0)';
+            }
+        });
+    }, { threshold: 0.1 });
+
+    document.querySelectorAll('.features-list > div').forEach(div => {
+        observer.observe(div);
+    });
+}
