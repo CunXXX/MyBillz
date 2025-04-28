916a0974ac0d32975dd2242a4c9e4f58b3726c3a
CunXXX <CunXXX@users.noreply.github.com>
2025-04-28 09:31:33 +0000

    deploy: 2bf9df02735abca1f0f88a1e794b07fad34c6a05

---
+++ Pages/FAQ.razor.js
@@ -0,0 +1,17 @@
+﻿
+export function toggleFaqAnswer(faqId) {
+    const answer = document.getElementById(faqId);
+    const arrow = answer.previousElementSibling.querySelector(".arrow");
+
+    if (!answer) return;
+
+    if (answer.style.maxHeight) {
+        // 收起
+        answer.style.maxHeight = null;
+        arrow.style.transform = "rotate(0deg)";
+    } else {
+        // 展開
+        answer.style.maxHeight = answer.scrollHeight + "px";
+        arrow.style.transform = "rotate(180deg)";
+    }
+}
